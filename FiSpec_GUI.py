78448eaf152797daf0cf9088dff09bccce4a3e8d
Jeffrey Klepka <jeffreyklepka@hotmail.de>
2022-02-11 16:17:12 +0100

    added Measurement Frame

--- FiSpec_GUI.py
+++ FiSpec_GUI.py
@@ -4,4 +4,7 @@ class FiSpec_GUI:
     def __init__(self, master):
         self.master=master
         self.frame_COM_select = Frame(master)
-        self.frame_COM_select.place(x=5,y=5)
+        self.frame_COM_select.place(x=5,y=5, width=400, height=400)
+
+        self.frame_Measurements = Frame(master)
+        self.frame_Measurements.place(x=5,y=405, width=400, height=400)
