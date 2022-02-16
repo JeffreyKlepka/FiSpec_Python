98edf539648ee379cd0727464d5fcfb1d98df684
Jeffrey Klepka <jeffreyklepka@hotmail.de>
2022-02-16 16:12:40 +0100

    calculating amplitude and wavelength now works

--- FiSpec_GUI.py
+++ FiSpec_GUI.py
@@ -1,4 +1,5 @@
-from tkinter import X, Frame
+from tkinter import Frame
+from tkinter import Label
 
 class FiSpec_GUI:
     def __init__(self, master):
@@ -10,10 +11,32 @@ class FiSpec_GUI:
         self.frame_start_meas.place(x=5,y=50, width=495, height=45)
 
         self.frame_start_spec = Frame(master)
-        self.frame_start_spec.place(x=5,y=105, width=495, height=45)
+        self.frame_start_spec.place(x=5,y=100, width=495, height=45)
+
+        self.frame_config = Frame(master)
+        self.frame_config.place(x=5,y=150, width=495, height=45)
         
         self.frame_Plot = Frame(master)
         self.frame_Plot.place(x=5,y=500, width=990, height=495)
 
         self.frame_Display_Data = Frame(master)
         self.frame_Display_Data.place(x=500,y=5, width=495, height=495)
+
+        self.label_Peak1 = Label(self.frame_Display_Data, text="Peak 1: ")
+        self.label_Peak1.place(x=10, y=10)
+        self.label_Peak2 = Label(self.frame_Display_Data, text="Peak 2: ")
+        self.label_Peak2.place(x=10, y=30)
+        self.label_Peak3 = Label(self.frame_Display_Data, text="Peak 3: ")
+        self.label_Peak3.place(x=10, y=50)
+        self.label_Peak4 = Label(self.frame_Display_Data, text="Peak 4: ")
+        self.label_Peak4.place(x=10, y=70)
+
+        self.label_Ampl1 = Label(self.frame_Display_Data, text="Amplitude 1: ")
+        self.label_Ampl1.place(x=200, y=10)
+        self.label_Ampl2 = Label(self.frame_Display_Data, text="Amplitude 2: ")
+        self.label_Ampl2.place(x=200, y=30)
+        self.label_Ampl3 = Label(self.frame_Display_Data, text="Amplitude 3: ")
+        self.label_Ampl3.place(x=200, y=50)
+        self.label_Ampl4 = Label(self.frame_Display_Data, text="Amplitude 4: ")
+        self.label_Ampl4.place(x=200, y=70)
+
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# from socket import timeout
+
 import tkinter as tk
 from tkinter import *
 from turtle import color
@@ -10,7 +10,7 @@ plt.use ('TkAgg')
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
-
+from functools import partial
 # import sys
 import time
 import numpy as np
@@ -25,18 +25,19 @@ root.geometry("1000x1000")
 measIsOn=False
 specIsOn=False
 
-xWL = []
+
+xWll=[]
+ySpec = []
 xLWL = []
 
-yAmpl = []
 
 fsG1 = fsG.FiSpec_GUI(root)
 plotSize = (4, 3)
 fig = Figure(plotSize)
 canvas = FigureCanvasTkAgg(fig, fsG1.frame_Plot)
-toolbar = NavigationToolbar2Tk(canvas, fsG1.frame_Plot)
-toolbar.pack(fill=tk.BOTH, expand=1)
-toolbar.update()
+# toolbar = NavigationToolbar2Tk(canvas, fsG1.frame_Plot)
+# toolbar.pack(fill=tk.BOTH, expand=1)
+# toolbar.update()
 
 fbg_count=4
 fbg_wavelength=[8200000, 8300000, 8400000, 8500000]
@@ -53,8 +54,6 @@ serialInst = serial.Serial()
     
 for onePort in ports:
     portList.append(str(onePort)[0:4])
-    # print(str(onePort))
-    
 try:
     portObj.set(portList[0])
 except:
@@ -119,6 +118,7 @@ def connectCOM():
         if dev_dec == "FiSpec FBG X100":
             dev_available=1
             print("Connected to device: " + dev_dec)
+            
         elif dev_dec == "FiSpec FBG X150":
             dev_available=1
             print("Connected to device: " + dev_dec)
@@ -127,7 +127,29 @@ def connectCOM():
     except:
         dev_available=0
         print("Error: No device found!")
+    
+    ledON()
+    checkWL()
 
+def checkWL():
+    global xWLL
+    try:
+        ser.flushInput()
+        ser.write("WLL>".encode())
+    except:
+        print("Error: WLL>")
+    
+    wll_data=ser.read_until('Ende')
+    wll_data_len = len(wll_data)
+    wll_data_len4=int(wll_data_len/4)
+    
+    for j in range(wll_data_len4-1):
+        try:
+            xWll.append((wll_data[4*j] + 256*wll_data[4*j+1] + 65536*wll_data[4*j+2] + 16777216*wll_data[4*j+3])/10000)
+        except:
+            pass
+
+    # print(xWll)
 connect_COM_Bt = Button(fsG1.frame_COM_select, text="Connect Port", command=connectCOM)
 connect_COM_Bt.place(x=150, y=2, width=120)
 
@@ -136,11 +158,14 @@ def configuration():
         ma1=fbg_wavelength[x]-fbg_halfwidth
         ma2=fbg_wavelength[x]+fbg_halfwidth
         conf_msg="Ke," + str(x) + "," + str(ma1) + "," + str(ma2) + ">"
-        conf_enc=conf_msg.encode()
         try:
-            ser.write(conf_enc)
+            ser.write(conf_msg.encode())
         except:
             print("Error: Configuration")
+        print(conf_msg)
+
+conf_Bt = Button(fsG1.frame_config, text="Configure", command=configuration)
+conf_Bt.place(x=150, y=2, width=120)
 
 def setChannel():
     setCh_msg="KA," + str(fbg_count) + ">"
@@ -152,9 +177,11 @@ def setChannel():
 
 def ledON():
     try:
-        ser.write("LED,1".encode())
+        ser.flushInput()
+        ser.write("LED,1>".encode())
     except:
         print("Error: LED")
+    time.sleep(2)
 
 def startInternal():
     try:
@@ -163,7 +190,7 @@ def startInternal():
         print("Error: Start internal Measurement")
 
 def integration():
-    integration_time=50000
+    integration_time=50000 #Eingabefeld, mit Button übernehmen
     setIt_msg="iz," + str(integration_time) + ">"
     setIt_enc=setIt_msg.encode()
     try:
@@ -176,7 +203,11 @@ def measurement():
     fbg_peak=[8200000, 8300000, 8400000, 8500000]
     fbg_ampl=[0, 0, 0, 0]
 
+    if measIsOn == False:
+        return
+
     if ser.is_open:
+        ser.flushInput()
         try:
             ser.write("P>".encode())
             print("Send P>")
@@ -185,52 +216,56 @@ def measurement():
     else:
         print("COM-Port is closed. Try again!")
         return
-    # Decode and process data
-
+    
     rcv_data=ser.read_until('Ende')
-    # rcv_data_dec=str(rcv_data)
-    # meas_val=rcv_data_dec.split('\\x') #separate HEX-Values in String
-    '''
-    for v in range(0, rcv_data)):
-        # x = meas_val[v].strip("'\\n\\r") #remove unwanted symbols
-        # meas_val[v]=x
-        # y = meas_val[v][0:2] #only store HEX-Value and ignore other characters
-        # meas_val[v]=y
-        if v > 0:
-            z = int(meas_val[v], 16) #convert HEX to DEC
-            meas_val[v]=z
-    del meas_val[0] #remove "b'" (first list-item)
-    '''
-    print(rcv_data)
-    # print(meas_val)
 
     for i in range(fbg_count):
         try:
-            fbg_peak[i] = rcv_data[8*i] + 256*rcv_data[8*i+1] + 65536*rcv_data[8*i+2] + 16777216*rcv_data[8*i+3]
-            fbg_ampl[i] = rcv_data[8*i+4] + 256*rcv_data[8*i+5] + 65536*rcv_data[8*i+6] + 16777216*rcv_data[8*i+7]
+            fbg_peak[i] = (rcv_data[8*i] + 256*rcv_data[8*i+1] + 65536*rcv_data[8*i+2] + 16777216*rcv_data[8*i+3])/10000
+            fbg_ampl[i] = (rcv_data[8*i+4] + 256*rcv_data[8*i+5] + 65536*rcv_data[8*i+6] + 16777216*rcv_data[8*i+7])/10000
             print("Messwerte " + str(i+1) + ": " + str(fbg_peak[i]) + ", " + str(fbg_ampl[i]))
         except:
             print("Error: Data incomplete")
+
+    fsG1.label_Peak1.configure(text="Peak 1: " + str(fbg_peak[0]))
+    fsG1.label_Peak2.configure(text="Peak 2: " + str(fbg_peak[1]))
+    fsG1.label_Peak3.configure(text="Peak 3: " + str(fbg_peak[2]))
+    fsG1.label_Peak4.configure(text="Peak 4: " + str(fbg_peak[3]))
+    fsG1.label_Ampl1.configure(text="Amplitude 1: " + str(fbg_ampl[0]))
+    fsG1.label_Ampl2.configure(text="Amplitude 2: " + str(fbg_ampl[1]))
+    fsG1.label_Ampl3.configure(text="Amplitude 3: " + str(fbg_ampl[2]))
+    fsG1.label_Ampl4.configure(text="Amplitude 4: " + str(fbg_ampl[3]))
     root.after(10000, measurement)
 
-meas_Bt = Button(fsG1.frame_start_meas, text="Start Measurement", command=measurement)
+def ctrlMeas():
+    global specIsOn, measIsOn
+
+    if measIsOn:
+        meas_Bt.config(text='Start Measurement')
+        measIsOn=False
+    else:
+        meas_Bt.config(text='Stop Measurement')
+        measIsOn=True
+        measurement()
+
+meas_Bt = Button(fsG1.frame_start_meas, text="Start Measurement", command=ctrlMeas)
 meas_Bt.place(x=150, y=0, width=120)
 
 def createSpectrum():
-    global fig, ax, xWL, yAmpl, specIsOn
+    global fig, ax, ySpec, specIsOn
 
     if specIsOn==False:
         return
     
     xLWL.clear() # Delete old lists and canvas
-    yAmpl.clear()
+    ySpec.clear()
     fig.clear()
     canvas.get_tk_widget().forget()
     # Send 's>'-command
     if ser.is_open:
+        ser.flushInput()
         try:
             ser.write("s>".encode())
-            print("Send s>")
         except:
             print("Error: Spectrum Request")
     else:
@@ -238,20 +273,47 @@ def createSpectrum():
         return
     
     spec_data=ser.read_until('Ende') # Read incoming data
-    spec_data_len = len(spec_data)
+    spec_data_len=len(spec_data)
+    spec_data_len2=int(spec_data_len/2)
+    # ySpec = []
     
-    xWL = np.arange(780, 900, 120/spec_data_len) # x-list has to have the same length as y-list
+    xWL = np.arange(780, 900, 120/spec_data_len2) # x-list has to have the same length as y-list
     for wl in xWL:
         xLWL.append(wl)
     
-    for ampl in spec_data:
-        yAmpl.append(spec_data[ampl]) # Save incoming array as a list
+    
+    for i in range(spec_data_len2):
+        try:
+            ySpec.append(spec_data[2*i]+256*spec_data[2*i+1])
+        except:
+            pass
+
+    # for ampl in spec_data:
+    #     yAmpl.append(spec_data[ampl]) # Save incoming array as a list
+    
     fig.tight_layout()
     ax = fig.add_subplot() # Configure and create plot
-    ax.plot(xWL, yAmpl, c='green')
+    
+    if len(xWll) > len(ySpec):
+        # x Liste kürzen, dann Plot
+        while(len(xWll) > len(ySpec)):
+            xWll.pop()
+        ax.plot(xWll, ySpec, c='green')
+    elif len(xWll) < len(ySpec):
+        # y Liste kürzen, dann Plot
+        while(len(xWll) < len(ySpec)):
+            ySpec.pop()
+        ax.plot(xWll, ySpec, c='green')
+    else:
+        ax.plot(xWll, ySpec, c='green')
+    
+    # ax.plot(xLWL, ySpec, c='green')
+    print(len(xWll))
+    print(len(ySpec))
     ax.set_title('Spectrum', fontsize=10)
     ax.set_facecolor('black')
-    # ax.set_xticks(np.arange(780, 900, step=20))
+    # ax.set_xlim(left=770, right=910)
+    ax.set_ylim(bottom=0, top=10000)
     ax.set_xlabel('Wavelength [nm]', fontsize=8)
     ax.set_ylabel('Amplitude', fontsize=8)
     ax.grid(color='yellow')
@@ -260,7 +322,7 @@ def createSpectrum():
     canvas.get_tk_widget().pack(fill=tk.BOTH, expand=1) # display plot as a widget of tkinter
     canvas.draw()
     
-    root.after(2000, createSpectrum)
+    root.after(10000, createSpectrum)
 
 def ctrlSpec():
     global specIsOn
@@ -277,6 +339,7 @@ spec_Bt = Button(fsG1.frame_start_spec, text="Start Spectrum", command=ctrlSpec)
 spec_Bt.place(x=150, y=0, width=120)   
 
 t_Controller = threading.Thread(target=checkCOMs).start()
-t_Meas_Controller = threading.Thread(target=measurement)
+t_Meas_Controller = threading.Thread(target=measurement).start()
 t_Spec_Controller = threading.Thread(target=createSpectrum).start()
+
 root.mainloop()
