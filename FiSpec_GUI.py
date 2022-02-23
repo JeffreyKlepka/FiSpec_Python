8773e65bc92f19114de511d96c8def1c8507718e
Jeffrey Klepka <jeffreyklepka@hotmail.de>
2022-02-23 16:29:48 +0100

    some minor changes regarding frames on the right side of the GUI

--- FiSpec_GUI.py
+++ FiSpec_GUI.py
@@ -6,7 +6,7 @@ class FiSpec_GUI:
         self.master=master
         # ----------
         self.frame_COM_select = Frame(master)
-        self.frame_COM_select.place(x=5,y=5, width=490, height=45)
+        self.frame_COM_select.place(x=5,y=5, width=490, height=40)
 
         self.portObj = StringVar(master)
         self.portList = ["0"]
@@ -86,28 +86,28 @@ class FiSpec_GUI:
         self.frame_Plot.place(x=5,y=500, width=990, height=495)
 
         self.frame_Display_Data = Frame(master)
-        self.frame_Display_Data.place(x=500,y=5, width=495, height=240)
+        self.frame_Display_Data.place(x=500,y=5, width=495, height=90)
 
         self.label_Peak1 = Label(self.frame_Display_Data, text="Peak 1: ")
-        self.label_Peak1.place(x=10, y=10)
+        self.label_Peak1.place(x=10, y=5)
         self.label_Peak2 = Label(self.frame_Display_Data, text="Peak 2: ")
-        self.label_Peak2.place(x=10, y=30)
+        self.label_Peak2.place(x=10, y=25)
         self.label_Peak3 = Label(self.frame_Display_Data, text="Peak 3: ")
-        self.label_Peak3.place(x=10, y=50)
+        self.label_Peak3.place(x=10, y=45)
         self.label_Peak4 = Label(self.frame_Display_Data, text="Peak 4: ")
-        self.label_Peak4.place(x=10, y=70)
+        self.label_Peak4.place(x=10, y=65)
 
         self.label_Ampl1 = Label(self.frame_Display_Data, text="Amplitude 1: ")
-        self.label_Ampl1.place(x=200, y=10)
+        self.label_Ampl1.place(x=200, y=5)
         self.label_Ampl2 = Label(self.frame_Display_Data, text="Amplitude 2: ")
-        self.label_Ampl2.place(x=200, y=30)
+        self.label_Ampl2.place(x=200, y=25)
         self.label_Ampl3 = Label(self.frame_Display_Data, text="Amplitude 3: ")
-        self.label_Ampl3.place(x=200, y=50)
+        self.label_Ampl3.place(x=200, y=45)
         self.label_Ampl4 = Label(self.frame_Display_Data, text="Amplitude 4: ")
-        self.label_Ampl4.place(x=200, y=70)
+        self.label_Ampl4.place(x=200, y=65)
         # ----------
         self.frame_wl = Frame(master)
-        self.frame_wl.place(x=500, y=250, width=495, height=245)
+        self.frame_wl.place(x=500, y=100, width=180, height=195)
 
         self.label_wl_1 = Label(self.frame_wl, text="Wavelength 1: ")
         self.label_wl_1.place(x=10, y=25)
